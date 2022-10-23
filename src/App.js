7a887d40aa25c4eff1e08f892d9a20773f9757ab
DiegoReyna00 <Diegoreyna31@Outlook.com>
2022-10-22 19:19:13 -0500

    1er commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Main } from './pages/main';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Login } from './pages/login';
+
+export const App = () => (
+  <div className="App">
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />}></Route>
+        <Route exact path="/main" element={<Main />}></Route>
+      </Routes>
+    </Router>
+  </div>
+);
